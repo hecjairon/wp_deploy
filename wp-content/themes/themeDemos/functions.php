ff21a6a10ee1df0e3edef6411a54eff35e087cf4
hecjairon <hecjairon@hotmail.com>
2023-09-11 04:27:32 -0300

    teste cfs

--- wp-content/themes/themeDemos/functions.php
+++ wp-content/themes/themeDemos/functions.php
@@ -5,9 +5,24 @@ function themeEditorDemos(){
         __( 'Editar Theme', 'themeEditorDemos' ),
         __( 'Editar Theme', 'themeEditorDemos' ),
         'edit_posts',
-        'managerAffiliate',
+        'themeEditorField',
         "fieldsEditorThemeDemo",
         'dashicons-groups',
         5
     );
+}
+
+function fieldsEditorThemeDemo(){
+    CFS()->create_field( [
+            'type' => 'select',
+            'input_name' => "my_name_is",
+            'options' => [
+                'choices' => [
+                    '==' => "teste",
+                    '!=' => "teste 2",
+                ],
+                'force_single' => true,
+            ],
+            'value' => "",
+        ] );
 }
